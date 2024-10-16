a9cbabc5d1224f4f46e9df2df95af3d02fab6a20
Marta Matsevych <martamatsevych@gmail.com>
2024-10-16 14:50:19 +0300

    make build

---
+++ index.f74cbf05.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");t.style.position="relative";var o=t.offsetWidth,s=t.offsetHeight,l=e.offsetWidth,i=e.offsetHeight;e.style.position="absolute",e.style.left="".concat((o-l)/2,"px"),e.style.top="".concat((s-i)/2,"px");
+//# sourceMappingURL=index.f74cbf05.js.map
